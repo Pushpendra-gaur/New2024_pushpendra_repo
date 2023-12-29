909394e98a7ef6033d9fb792c888ffeedc59d34a
Er Pushpendra kumar gaur <pkg201297@gmail.com>
2023-12-29 11:28:22 +0530

    remove space

--- src/Page_class/Home_page.java
+++ src/Page_class/Home_page.java
@@ -8,11 +8,7 @@ public class Home_page extends Base_liebrary {
 
 	
 	
-	@FindBy(xpath = "//button[@aria-label='Dismiss sign-in info.']")
-	private WebElement Cross_icon;
 	
-	@FindBy(xpath = "//span[text()='Flights']")
-	private WebElement flights;
 	
 	public Home_page()
 	{
@@ -22,33 +18,12 @@ public class Home_page extends Base_liebrary {
 	
 	public void booking_flith() throws InterruptedException
 	{
-		
-		System.out.println("pushpendra gaur");
-		//try {	
-		
-		Thread.sleep(2000);
-		
-		System.out.println("ruby");
-		Cross_icon.click();
-		
-		System.out.println("ankit gaur");
-		
-		Thread.sleep(2000);
-		flights.click();
+	
 		
 		
-		System.out.println("ankit gaur");
-		}
 		
-		//catch (Exception e) {
-			
-			//e.printStackTrace();
-			
-			//System.out.println("pushpendra");
-			
-		//}
 		
- 
+	}
 	}
 	
 	
